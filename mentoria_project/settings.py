6e8a7b32550bb62af48c395905948f772218fd28
Mayer0212 <gigiomayer@gmail.com>
2026-01-19 16:54:21 -0300

    consetarndo o settings

--- mentoria_project/settings.py
+++ mentoria_project/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-n*vwxtav_=$b8pf46eino0qppbhp&8#5zasqm+mch-t^lmvca+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mayer.pythonanywhere.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
