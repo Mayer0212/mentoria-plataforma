76d98ccbbeb1d05f18f7438b9c66d66887753510
Mayer0212 <gigiomayer@gmail.com>
2026-01-26 04:47:53 -0300

    Migração para Linux: Ajuste de settings e normalização de quebras de linha

--- mentoria_project/settings.py
+++ mentoria_project/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-n*vwxtav_=$b8pf46eino0qppbhp&8#5zasqm+mch-t^lmvca+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['mayer.pythonanywhere.com', 'localhost', '127.0.0.1']
 
